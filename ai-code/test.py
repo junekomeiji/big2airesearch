b26cee6fd2605b67beaf8717feaff4961b75ee9f
mlemko <isamfz445@gmail.com>
2023-06-02 19:51:36 +1000

    finished testing file

--- ai-code/test.py
+++ ai-code/test.py
@@ -2,6 +2,9 @@ import tensorflow as tf
 import numpy as np
 import random
 from deap import base, creator, tools, algorithms
+from pygad.kerasga import model_weights_as_matrix
+import big2text
+import pickle
 
 def varAnd(population, toolbox, cxpb, mutpb):
     r"""Part of an evolutionary algorithm applying only the variation part
@@ -120,9 +123,22 @@ def eaSimple(population, toolbox, cxpb, mutpb, ngen, stats=None,
 
     # Evaluate the individuals with an invalid fitness
     invalid_ind = [ind for ind in population if not ind.fitness.valid]
-    fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
-    for ind, fit in zip(invalid_ind, fitnesses):
-        ind.fitness.values = fit
+    # MODIFICATIONS HERE: Fitness is done in groups of 4 instead of individually.
+    group_length = int(np.floor_divide(len(invalid_ind), 4))
+    invalid_indgroups = []
+    for i in range(4):
+        indgroup = []
+        for x in range(group_length):
+            indgroup.append(invalid_ind[i * group_length + x])
+        invalid_indgroups.append(indgroup)
+    
+    fitnesses = map(toolbox.evaluate, invalid_indgroups[0], invalid_indgroups[1], invalid_indgroups[2], invalid_indgroups[3])
+    for n, fitness in enumerate(fitnesses):
+        invalid_ind[n].fitness.values = fitness[0]
+        invalid_ind[n + group_length].fitness.values = fitness[1]
+        invalid_ind[n + group_length * 2].fitness.values = fitness[2]
+        invalid_ind[n + group_length * 3].fitness.values = fitness[3]
+        
 
     if halloffame is not None:
         halloffame.update(population)
@@ -142,9 +158,21 @@ def eaSimple(population, toolbox, cxpb, mutpb, ngen, stats=None,
 
         # Evaluate the individuals with an invalid fitness
         invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
-        fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
-        for ind, fit in zip(invalid_ind, fitnesses):
-            ind.fitness.values = fit
+        # MODIFICATIONS HERE: Fitness is done in groups of 4 instead of individually.
+        group_length = int(np.floor_divide(len(invalid_ind), 4))
+        invalid_indgroups = []
+        for i in range(4):
+            indgroup = []
+            for x in range(group_length):
+                indgroup.append(invalid_ind[i * group_length + x])
+            invalid_indgroups.append(indgroup)
+        
+        fitnesses = map(toolbox.evaluate, invalid_indgroups[0], invalid_indgroups[1], invalid_indgroups[2], invalid_indgroups[3])
+        for n, fitness in enumerate(fitnesses):
+            invalid_ind[n].fitness.values = fitness[0]
+            invalid_ind[n + group_length].fitness.values = fitness[1]
+            invalid_ind[n + group_length * 2].fitness.values = fitness[2]
+            invalid_ind[n + group_length * 3].fitness.values = fitness[3]
 
         # Update the hall of fame with the generated individuals
         if halloffame is not None:
@@ -163,18 +191,184 @@ def eaSimple(population, toolbox, cxpb, mutpb, ngen, stats=None,
 
 def model_build():
     model = tf.keras.Sequential()
-    model.add(tf.keras.Input(shape=(140,)))
-    model.add(tf.keras.layers.Dense(104, activation="relu"))
-    model.add(tf.keras.layers.Dense(104, activation="relu"))
-    model.add(tf.keras.layers.Dense(13, activation="softmax"))
+    model.add(tf.keras.Input(shape=(141,)))
+    model.add(tf.keras.layers.Dense(107, activation="relu"))
+    model.add(tf.keras.layers.Dense(107, activation="relu"))
+    model.add(tf.keras.layers.Dense(13, activation="sigmoid"))
     
     # these are not used for this model, but are needed for compilation
     model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
     return model
 
-def evaluate_game(ind1, ind2, ind3, ind4):
-    pass
+def generate_input(my_deck, prev_deck, prev_hand):
+    inp_vector = []
+    for x in range(13):
+        try:
+            card = big2text.Card.num_to_vector(my_deck[x])
+        except IndexError:
+            card = [0, 0]
+        inp_vector.extend(card)
+    for x in range(52):
+        try:
+            card = big2text.Card.num_to_vector(prev_deck[x])
+        except IndexError:
+            card = [0,0]
+        inp_vector.extend(card)
+    for x in range(5):
+        try:
+            card = big2text.Card.num_to_vector(prev_hand[x])
+        except:
+            card = [0,0]
+        inp_vector.extend(card)
+    return inp_vector
 
+def predictions_to_hand(prediction, my_deck):
+    hand = []
+    for x, pred in zip(range(13), prediction):
+        if pred >= 0.5:
+            try:
+                hand.append(my_deck[x])
+            except IndexError:
+                pass
+    hand.sort()
+    return hand
+
+def evaluate_game(ind1, ind2, ind3, ind4):
+    big2text.init()
+    
+    # Building the players
+    models = []
+    inds = [ind1, ind2, ind3, ind4]
+    
+    for ind in inds:
+        model = model_build()
+        model.set_weights(model_weights_as_matrix(model, ind))
+        models.append(model)
+    
+    fitnesses = [0, 0, 0, 0]
+    exit = False
+    first_hand = True
+    consec_skips = 0
+    while(not exit):
+        # Reading game state
+        file = open("gamestate", "r") 
+        save = file.readlines()
+        file.close()
+        skips = int(save[8].strip(" \n"))
+        
+        prev_deck = []
+        if save[5].strip(" \n") != "-1":
+            prev_deck = map(int, save[5].strip(" \n").split(','))
+            
+        prev_empty = save[6].strip(" \n") == "-1"
+        prev_hand = []
+        if prev_empty:
+            prev_hand = map(int, save[6].strip(" \n").split(','))
+        
+        playernum = int(save[7].strip(" \n"))
+        my_deck = map(int, save[playernum].strip(" \n").split(','))
+        
+        # Mapping relevant game variables to input vector
+        inp_vector = generate_input(my_deck, prev_deck, prev_hand)
+        inp_vector.append(int(save[8].strip[" \n"]))
+        
+        #PREDICTION TIME
+        prediction = models[playernum - 1].predict(inp_vector)
+        
+        # Mapping prediction into readable hand
+        hand = predictions_to_hand(prediction, my_deck)
+        
+        # verification and retry
+        fails = 0
+        while (fails < 5):
+            hand_cards = map(big2text.Card, hand)
+            my_deck_cards = map(big2text.Card, my_deck)
+            prev_hand_cards = map(big2text.Card, prev_hand)
+            if big2text.verify(hand_cards, my_deck_cards, prev_hand_cards, prev_empty, first_hand) != 0:
+                # try a different prediction by shuffling cards in hand
+                fails += 1
+                random.shuffle(my_deck)
+                inp_vector = generate_input(my_deck, prev_deck, prev_hand)
+                inp_vector.append(int(save[8].strip[" \n"]))
+                prediction = models[playernum - 1].predict(inp_vector)
+                hand = predictions_to_hand(prediction, my_deck)
+            else:
+                break
+        else:
+            # if the model fails to produce a valid hand, force to skip
+            # except on first move, where 3 of diamonds is forced to be put down
+            if first_hand:
+                hand_cards = [big2text.Card(0)]
+            else:
+                hand_cards = []
+        
+        # Resets skip count if there's a hand.
+        if len(hand_cards) == 0:
+            consec_skips += 1
+        else:
+            consec_skips = 0
+            
+        # Update the gamestate
+        big2text.execute_move(hand_cards, playernum)
+        
+        # Check for winners
+        winner = big2text.are_we_done_yet()
+        if winner != 0:
+            exit = True
+        
+        # Check for too many skips
+        if consec_skips > 12:
+            exit = True
+
+    # Once finished, calculate fitness
+    # When exited because skips, everyone gets a negative fitness
+    file = open("gamestate", "r")
+    save = file.readlines()
+    file.close()
+    
+    card_count = [0, 0, 0, 0]
+    for x in range(4):
+        card_count[x] = len(save[x + 1].split(','))
+    
+    for x in range(4):
+        y = x + 1
+        if y == winner:
+            for z in range(4):
+                if z != x:
+                    fitnesses[x] += card_count[z]
+        else:
+            fitnesses[x] = -card_count[x]
+    
+    # End of evaluation
+    return fitnesses
+        
+model = model_build()
+ind_size = model.count_params()
 
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMax)
+toolbox = base.Toolbox()
+toolbox.register("weight_bin", random.random) # Generating random weights
+toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.weight_bin, n=ind_size)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+toolbox.register("mate", tools.cxTwoPoint)
+toolbox.register("mutate", tools.mutFlipBit, indpb=0.01)
+toolbox.register("select", tools.selTournament, tournsize=3)
+toolbox.register("evaluate", evaluate_game)
+
+stats = tools.Statistics(lambda ind: ind.fitness.values)
+stats.register("Mean", np.mean)
+stats.register("Max", np.max)
+stats.register("Min", np.min)
+
+pop = toolbox.population(n=100)   #n = No. of individual in a population
+hof = tools.HallOfFame(1)
+
+#Run GA with crossover probability 0.5, mutation probability 0.01 and for 30 generations
+pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.01, ngen=30, halloffame=hof, stats=stats)
+
+# takes the best individual of the latest generation and saves it as an object
+best_pop = sorted(pop, key=lambda ind: ind.fitness, reverse=True)[0]
+with open("big2_model.pkl", "wb") as md_file:
+    pickle.dump(best_pop, md_file)
